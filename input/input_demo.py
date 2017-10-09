c5237ca6e86852e1ef2821161f6b1beb97dfecf4
Neville Oronni <nevooronni@gmail.com>
2017-10-09 20:54:49 +0300

    used a break and continue keywords

--- input/input_demo.py
+++ input/input_demo.py
@@ -34,3 +34,27 @@ players = 11
 while players >= 5 :
     print("The remaining players are",players)
     players -= 1 #loop will run again
+
+
+number = 0
+while True:
+    print("I love candy "+ str(number))
+    number +=1
+    if number == 7 :
+        break
+
+'''
+in a team of members 20, some numbers are taken and want to display the numbers
+that are not taken so others don't pick the picked numbers
+'''
+
+# taken numbers
+numTaken = [3,5,7,11,13]
+
+print("Available numbers")
+
+# loop
+for i in range(1,21):
+    if i in numTaken:
+        continue #continue does not print the numbers under the numTaken list
+    print(i) #prints numbers from 1-20 but skip over the numTaken list.
