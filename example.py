261dea5a5f19f8907ee532346a7247770cf8fd3d
Neville Oronni <nevooronni@gmail.com>
2017-10-09 12:55:23 +0300

    included everything

---
+++ example.py
@@ -0,0 +1,12 @@
+import random
+
+# we can generate a random number using the randint method
+
+random_number = random.randint(0,10)
+
+print(random_number)
+
+r = random.randint(0,10)
+circumference = (2 * 3.14) * r
+
+print(circumference)
